96971ea6b52d5b799067492a2c6800c8840ba17b
abhinand <abhinandraj0111@gmail.com>
2024-03-10 14:20:42 +0530

    webcam detect moving objects

--- main.py
+++ main.py
@@ -15,7 +15,15 @@ while True:
 
     if first_frame is None:
         first_frame =gray_frame_gau
-
+    delta_frame = cv2.absdiff(first_frame,gray_frame_gau)#difference between first frame and other frame
+    thresh_frame= cv2.threshold(delta_frame,60, 255, cv2.THRESH_BINARY)[1]
+    dil_frame= cv2.dilate(thresh_frame,None,iterations=2)#to remove noise from thresh_frame. Higher the iteration number more processing will be applied
+    contours, check=cv2.findContours(dil_frame,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours:
+        if cv2.contourArea(contour) <7000:
+            continue
+        x,y,w,h= cv2.boundingRect(contour)
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0))
     cv2.imshow("My Video",frame)# to show the video from camera
     key= cv2.waitKey(1)
     if key== ord("q"):# to exit from camera we press q
