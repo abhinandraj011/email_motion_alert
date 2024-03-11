ab8201753b4285d6d54a5700ec0ea74e7ec926fc
abhinand <abhinandraj0111@gmail.com>
2024-03-11 18:08:48 +0530

    threading implemented

---
+++ emailing.py
@@ -0,0 +1,22 @@
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+PASSWORD="artrgaqpldzzfvod"
+SENDER="projecttest0111@gmail.com"
+RECEIVER="abhinandraj011@gmail.com"
+def send_email(image_path):
+    email_message= EmailMessage()
+    email_message["Subject"]="Movement Detected!"
+    email_message.set_content("Hey, Just found out a Movement")
+
+    with open(image_path, "rb") as file:
+        content=file.read()
+        email_message.add_attachment(content,maintype="image",subtype=imghdr.what(None, content))
+    gmail= smtplib.SMTP("smtp.gmail.com",587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER,PASSWORD)
+    gmail.sendmail(SENDER,RECEIVER,email_message.as_string())
+    gmail.quit()
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
+import os
+from threading import Thread
 import cv2
 import time
+from emailing import send_email
+import glob
+
 
 
 video=cv2.VideoCapture(0) #to use the main camera use 0 and to use secondary camera write 1
 time.sleep(1)# to wait for the camera to load
 first_frame= None
+count=1
+
+status_list=[]
+#remove all images from the folder
+def clean_folder():
+    images=glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 while True:
+    status = 0
     check, frame=video.read()
     gray_frame=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)#the frame is converted to gray
     # to reduce size and we Use COLOR_BGR2GRAY algorithm to convert each pixel to gray
@@ -23,9 +38,28 @@ while True:
         if cv2.contourArea(contour) <7000:
             continue
         x,y,w,h= cv2.boundingRect(contour)
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0))
+        rectangle =cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0))
+        if rectangle.any():
+            status=1
+            cv2.imwrite(f"images/{count}.png",frame)
+            count=count+1
+            all_images=glob.glob("images/*.png")
+            index=int(len(all_images)/2)
+            image_with_object=all_images[index]
+    status_list.append(status)
+    status_list=status_list[-2:]
+
+    if status_list[0] == 1 and status_list[1]==0:
+        send_email_thread= Thread(target=send_email,args=(image_with_object, ))
+        send_email_thread.daemon=True
+        clean_folder_thread=Thread(target=clean_folder)
+        clean_folder_thread.daemon=True
+
+        send_email_thread.start()
+    print(status_list)
     cv2.imshow("My Video",frame)# to show the video from camera
     key= cv2.waitKey(1)
     if key== ord("q"):# to exit from camera we press q
         break
 video.release()
+clean_folder_thread.start()
